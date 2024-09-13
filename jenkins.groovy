794fdaeea1194fa80a7b33dff1feacae763b3343
roiavivi <67497305+roiavivi@users.noreply.github.com>
2024-09-13 17:18:53 +0300

    Create jenkins.groovy

---
+++ jenkins.groovy
@@ -0,0 +1,18 @@
+pipelineJob("p1") {
+   description("Pipeline P1 is cool.")
+   displayName("CI-DSL")
+   definition {
+      cpsScm {
+         scm {
+            git {
+               remote {
+                  url 'https://github.com/roiavivi/AppCode.git'
+               }
+               branch('main')
+            }
+         }
+         // This will be cloned and then ran when Pipeline P1 is ran
+         scriptPath('./Jenkinsfile')
+      }
+   }
+}
