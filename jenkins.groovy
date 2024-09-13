1b9fbd9ada98b938731cdec695371fefdc7296d3
roiavivi <67497305+roiavivi@users.noreply.github.com>
2024-09-13 17:37:11 +0300

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,18 +1,5 @@
-pipelineJob("p1") {
-   description("Pipeline P1 is cool.")
-   displayName("CI-DSL")
-   definition {
-      cpsScm {
-         scm {
-            git {
-               remote {
-                  url 'https://github.com/roiavivi/AppCode.git'
-               }
-               branch('main')
-            }
-         }
-         // This will be cloned and then ran when Pipeline P1 is ran
-         scriptPath('Jenkinsfile')
-      }
-   }
+job('example') {
+  steps {
+    shell('echo Hello World!')
+  }
 }
